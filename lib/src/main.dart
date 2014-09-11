4d7108590f28edf9aa4f280ffbe4dacd8519a436
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-11 16:57:39 -0400

    Cool Stuff

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -7,19 +7,30 @@ final List<Command> commands = [
 void main(List<String> args) {
   var argp = new ArgParser(allowTrailingOptions: false);
   
+  argp.addFlag("help", abbr: "h", help: "Prints this Help Message", defaultsTo: false, negatable: false);
+  
   for (var cmd in commands) {
-    argp.addCommand(cmd.name, cmd.arguments);
+    var it = cmd.arguments;
+    it.addFlag("help", abbr: "h", help: "Prints this Help Message", defaultsTo: false, negatable: false);
+    argp.addCommand(cmd.name, it);
   }
   
   var opts = argp.parse(args);
   
-  if (opts.command == null) {
+  if (opts.command == null && opts.options.contains("help")) {
     print("Usage: darn <command> [args]");
     print(argp.getUsage());
     exit(0);
   }
   
   var cmd = commands.firstWhere((cmd) => cmd.name == opts.command.name);
+  
+  
+  if (opts.command.options.contains("help")) {
+    print("Usage: darn ${cmd.name} [args]");
+    print(argp.commands[opts.command.name].getUsage());
+    exit(0);
+  }
   
   cmd.run(opts.command);
 }
