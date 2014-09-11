ef8e5d54e9e824a7481771ab21064d8edbee5d66
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-11 17:03:47 -0400

    Bug Fixes

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -7,30 +7,22 @@ final List<Command> commands = [
 void main(List<String> args) {
   var argp = new ArgParser(allowTrailingOptions: false);
   
-  argp.addFlag("help", abbr: "h", help: "Prints this Help Message", defaultsTo: false, negatable: false);
+  argp.addFlag("help", abbr: "h", help: "Prints this Help Message", negatable: false, defaultsTo: false);
   
   for (var cmd in commands) {
     var it = cmd.arguments;
-    it.addFlag("help", abbr: "h", help: "Prints this Help Message", defaultsTo: false, negatable: false);
     argp.addCommand(cmd.name, it);
   }
   
   var opts = argp.parse(args);
   
-  if (opts.command == null && opts.options.contains("help")) {
+  if (opts.command == null || opts['help']) {
     print("Usage: darn <command> [args]");
     print(argp.getUsage());
     exit(0);
   }
   
   var cmd = commands.firstWhere((cmd) => cmd.name == opts.command.name);
-  
-  
-  if (opts.command.options.contains("help")) {
-    print("Usage: darn ${cmd.name} [args]");
-    print(argp.commands[opts.command.name].getUsage());
-    exit(0);
-  }
   
   cmd.run(opts.command);
 }
